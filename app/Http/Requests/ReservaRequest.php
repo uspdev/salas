1f36e9fe398ad739f5e06dfb2543d861bdea8ba1
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-01-11 14:28:45 -0300

    validação das datas

--- app/Http/Requests/ReservaRequest.php
+++ app/Http/Requests/ReservaRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Carbon\Carbon;
 
 class ReservaRequest extends FormRequest
 {
@@ -25,10 +26,10 @@ class ReservaRequest extends FormRequest
     {
         return [
             'nome'           => 'required',
-            'data_inicio'    => 'required',
-            'data_fim'       => 'required',
-            'horario_inicio' => 'required',
-            'horario_fim'    => 'required',
+            'data_inicio'    => 'required|date_format:d/m/Y',
+            'data_fim'       => 'required|date_format:d/m/Y|after_or_equal:data_inicio',
+            'horario_inicio' => 'required|date_format:H:i:s',
+            'horario_fim'    => 'required|date_format:H:i:s|after:horario_inicio',
             'cor'            => 'nullable',
             'sala_id'        => 'required',
             'full_day_event' => 'required',
@@ -38,16 +39,18 @@ class ReservaRequest extends FormRequest
 
     public function messages()
     {
-    return [
-        'nome.required'           => 'O nome não pode ficar em branco.',
-        'data_inicio.required'    => 'A data de início não pode ficar em branco.',
-        'data_fim.required'       => 'A data de fim não pode ficar em branco.',
-        'horario_inicio.required' => 'O horário de início não pode ficar em branco.',
-        'horario_fim.required'    => 'O horário de fim não pode ficar em branco.',
-        'sala_id.required'        => 'Selecione uma sala.',
-        'full_day_event.required' => 'Selecione uma opção para "Evento de dia inteiro".',
-    ];
+        return [
+            'nome.required'           => 'O nome não pode ficar em branco.',
+            'data_inicio.required'    => 'A data de início não pode ficar em branco.',
+            'data_fim.required'       => 'A data de fim não pode ficar em branco.',
+            'horario_inicio.required' => 'O horário de início não pode ficar em branco.',
+            'horario_fim.required'    => 'O horário de fim não pode ficar em branco.',
+            'sala_id.required'        => 'Selecione uma sala.',
+            'full_day_event.required' => 'Selecione uma opção para "Evento de dia inteiro".',
+        ];
     }
+
+
 
 
 }
