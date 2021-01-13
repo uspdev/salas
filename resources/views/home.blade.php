7a0e2a844d42980c77a3f5700b8c002ecd8f994d
Juliana Saotome <saotome@usp.br>
2021-01-13 16:52:56 -0300

    Modificação no IndexController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('main')
 @section('content')
 <h2>Sistema de Reserva de Salas</h2>
-Bem-vinde!
+<img src="https://media1.tenor.com/images/f5d8d14440714e60693ac381e80b4b14/tenor.gif" width="15%">
 @endsection
