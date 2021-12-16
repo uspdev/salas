60cebdab4a799539898301b821e4ae1993fe136e
Saotome <saotome@usp.br>
2021-12-16 17:05:39 -0300

    Old no array de repeat_days]

--- resources/views/reserva/partials/checkFlex.blade.php
+++ resources/views/reserva/partials/checkFlex.blade.php
@@ -1,6 +1,5 @@
 <div class="card">
     <div class="card-body">
-        <input class="form-check-input me-1" type="{{ $type ?? '' }}" name="{{ $name ?? '' }}" id="id{{ $label }}" value="{{ $value ?? '' }}"><label for="id{{ $label ?? '' }}">{{ $label ?? '' }}</label>
+        <input class="form-check-input me-1" type="{{ $type ?? '' }}" name="{{ $name ?? '' }}" id="id{{ $label }}" value="{{ $value ?? '' }}" @if(old('repeat_days.'.$value) == $value)  checked  @endif><label for="id{{ $label ?? '' }}">{{ $label ?? '' }}</label>
      </div>
 </div>
-
