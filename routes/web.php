dfb6c4a36c23ee34a500282df3477e9a6c5f5999
Saotome <saotome@usp.br>
2022-04-18 19:14:29 -0300

    feat(routes): adicionar novas rotas à web.php
    Rotas para edição em massa de reservas

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,32 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReservaController; 
-use App\Http\Controllers\SalaController; 
-use App\Http\Controllers\CategoriaController; 
-use App\Http\Controllers\RecursoController; 
-use App\Http\Controllers\IndexController; 
-use App\Http\Controllers\LoginController; 
+use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\GeneralSettingsController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\RecursoController;
+use App\Http\Controllers\ReservaController;
+use App\Http\Controllers\SalaController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/reservas/my', [ReservaController::class,'my']);
-
+Route::get('/reservas/my', [ReservaController::class, 'my']);
 Route::resource('/reservas', ReservaController::class);
+Route::get('/reservas/{reserva}/editAll', [ReservaController::class, 'editAll']);
+Route::post('/reservas/updateAll/{reserva}', [ReservaController::class, 'updateAll']);
+
 Route::resource('/salas', SalaController::class);
+
 Route::resource('/recursos', RecursoController::class);
+
 Route::resource('/categorias', CategoriaController::class);
+Route::post('/categorias/adduser/{categoria}', [CategoriaController::class, 'addUser']);
+Route::delete('/categorias/removeuser/{categoria}/{user}', [CategoriaController::class, 'removeUser']);
 
-Route::post('/categorias/adduser/{categoria}', [CategoriaController::class,'addUser']);
-Route::delete('/categorias/removeuser/{categoria}/{user}', [CategoriaController::class,'removeUser']);
-
-# Logs  
+// Logs
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index')->middleware('can:admin');
 
-# settings
-Route::get('/settings', [GeneralSettingsController::class,'show']);
-Route::post('/settings', [GeneralSettingsController::class,'update']);
+// settings
+Route::get('/settings', [GeneralSettingsController::class, 'show']);
+Route::post('/settings', [GeneralSettingsController::class, 'update']);
 
-# index:
-Route::get('/', [IndexController::class,'home']);
+// index:
+Route::get('/', [IndexController::class, 'home']);
