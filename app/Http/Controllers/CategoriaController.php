6dd21f57b7652b69833cb99bbe19bdd0cbc280c6
Saotome <saotome@usp.br>
2022-04-18 19:06:57 -0300

    feat(salas): calendário por sala agora exibe as salas agrupadas por categoria
    Por consequência, foi deletado a página do índice de categorias

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,29 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoriaRequest;
 use App\Models\Categoria;
 use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Requests\CategoriaRequest;
-use Illuminate\Support\Facades\Validator;
 use Uspdev\Replicado\Pessoa;
 
 class CategoriaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $categorias = Categoria::all();
-
-        return view('categoria.index', [
-            'categorias' => $categorias
-        ]);
-    }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -33,16 +18,17 @@ class CategoriaController extends Controller
     public function create()
     {
         $this->authorize('admin');
-        
+
         return view('categoria.create', [
-            'categoria' => new Categoria,
+            'categoria' => new Categoria(),
         ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(CategoriaRequest $request)
@@ -51,27 +37,26 @@ class CategoriaController extends Controller
 
         $validated = $request->validated();
         $categoria = Categoria::create($validated);
-        request()->session()->flash('alert-info', 'Categoria criada com sucesso.');
+        request()->session()->flash('alert-sucess', 'Categoria criada com sucesso.');
+
         return redirect("/categorias/{$categoria->id}");
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Categoria  $categoria
      * @return \Illuminate\Http\Response
      */
     public function show(Categoria $categoria)
     {
-        return view('categoria.show',[
-            'categoria' => $categoria
+        return view('categoria.show', [
+            'categoria' => $categoria,
         ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Categoria  $categoria
      * @return \Illuminate\Http\Response
      */
     public function edit(Categoria $categoria)
@@ -79,31 +64,31 @@ class CategoriaController extends Controller
         $this->authorize('admin');
 
         return view('categoria.edit', [
-            'categoria' => $categoria
+            'categoria' => $categoria,
         ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Categoria  $categoria
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(CategoriaRequest $request, Categoria $categoria)
     {
         $this->authorize('admin');
-        
+
         $validated = $request->validated();
         $categoria->update($validated);
-        request()->session()->flash('alert-info', 'Categoria atualizada com sucesso.');
+        request()->session()->flash('alert-success', 'Categoria atualizada com sucesso.');
+
         return redirect("/categorias/{$categoria->id}");
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Categoria  $categoria
      * @return \Illuminate\Http\Response
      */
     public function destroy(Categoria $categoria)
@@ -111,53 +96,53 @@ class CategoriaController extends Controller
         $this->authorize('admin');
 
         $categoria->delete();
-        request()->session()->flash('alert-info', 'Categoria excluída com sucesso.');
-        return redirect('/categorias');
+        request()->session()->flash('alert-success', 'Categoria excluída com sucesso.');
+
+        return redirect('/salas');
     }
 
     public function addUser(Request $request, Categoria $categoria)
     {
         $this->authorize('admin');
 
-        # é um número USP válido?
+        // é um número USP válido?
         $pessoa = Pessoa::dump($request->codpes);
-        if(!$pessoa) {
+        if (!$pessoa) {
             /* dd('Não encontrei ess apessoa chara') */
             request()->session()->flash('alert-danger', 'Número USP inválido');
+
             return redirect("/categorias/{$categoria->id}");
         }
 
-        # Cria um objeto para o usuário em questão
-        $user = User::where('codpes',$request->codpes)->first();
-        if(!$user) {
-            $user = new User;
+        // Cria um objeto para o usuário em questão
+        $user = User::where('codpes', $request->codpes)->first();
+        if (!$user) {
+            $user = new User();
             $user->codpes = $request->codpes;
             $user->name = $pessoa['nompes'];
             $user->email = Pessoa::emailusp($request->codpes);
             $user->save();
         }
 
-        # não pode existir na tabela categoria_users uma instância
-        # com o user_id e a categoria_id solicitados.
-        if(!$categoria->users->contains($user)) {
+        // não pode existir na tabela categoria_users uma instância
+        // com o user_id e a categoria_id solicitados.
+        if (!$categoria->users->contains($user)) {
             $categoria->users()->attach($user);
             request()->session()->flash('alert-success', "{$user->name} cadastrado(a) em {$categoria->nome}");
-        }
-        else {
+        } else {
             request()->session()->flash('alert-warning', "{$user->name} já está cadastrado(a) em {$categoria->nome}");
         }
-        
+
         return redirect("/categorias/{$categoria->id}");
     }
 
     public function removeUser(Request $request, Categoria $categoria, User $user)
-    {    
+    {
         $this->authorize('admin');
 
         $categoria->users()->detach($user->id);
-        request()->session()->flash('alert-danger', "{$user->name} foi excluído(a) de {$categoria->nome}");
+        request()->session()->flash('alert-success', "{$user->name} foi excluído(a) de {$categoria->nome}");
+
         return redirect("/categorias/{$categoria->id}");
     }
-
-
 }
--- app/Http/Controllers/SalaController.php
+++ app/Http/Controllers/SalaController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Sala;
-use App\Models\Categoria;
-use Illuminate\Http\Request;
 use App\Http\Requests\SalaRequest;
-use Illuminate\Support\Facades\Validator;
+use App\Models\Categoria;
+use App\Models\Sala;
+use Illuminate\Http\Request;
 
 class SalaController extends Controller
 {
@@ -17,16 +16,10 @@ class SalaController extends Controller
      */
     public function index(Request $request)
     {
-        if(isset(request()->search)){
-            $salas = Sala::where('nome', 'LIKE',"%{$request->search}%")->paginate(5);
-        }
+        $categorias = Categoria::all();
 
-        else{
-        $salas = Sala::orderBy('nome', 'asc')->paginate(20);
-        }
-
-        return view('sala.index',[
-            'salas' => $salas
+        return view('sala.index', [
+            'categorias' => $categorias,
         ]);
     }
 
@@ -36,19 +29,20 @@ class SalaController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
-    {   
+    {
         $this->authorize('admin');
 
         return view('sala.create', [
-            'sala' => new Sala,
-            'categorias' => Categoria::all()
+            'sala' => new Sala(),
+            'categorias' => Categoria::all(),
         ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(SalaRequest $request)
@@ -57,22 +51,22 @@ class SalaController extends Controller
 
         $validated = $request->validated();
         $sala = Sala::create($validated);
-        request()->session()->flash('alert-info', 'Sala criada com sucesso.');
+        request()->session()->flash('alert-success', 'Sala criada com sucesso.');
+
         return redirect("/salas/{$sala->id}");
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Sala  $sala
      * @return \Illuminate\Http\Response
      */
     public function show(Sala $sala)
     {
-        # pegando as reservas das salas
+        // pegando as reservas das salas
         $events = [];
 
-        foreach($sala->reservas as $reserva) {
+        foreach ($sala->reservas as $reserva) {
             $events[] = \Calendar::event(
                 $reserva->nome,
                 false, //full day event?
@@ -81,7 +75,7 @@ class SalaController extends Controller
                 0, //optionally, you can specify an event ID,
                 [
                     'color' => $reserva->cor,
-                    'url' => '/reservas/' . $reserva->id ,
+                    'url' => '/reservas/'.$reserva->id,
                 ],
             );
         }
@@ -89,19 +83,18 @@ class SalaController extends Controller
         $calendar = \Calendar::addEvents($events)
             ->setOptions([
                 'firstDay' => 1,
-                'defaultView' => 'agendaWeek'
+                'defaultView' => 'agendaWeek',
         ]);
 
-        return view('sala.show',[
-            'sala'     => $sala,
-            'calendar' => $calendar
+        return view('sala.show', [
+            'sala' => $sala,
+            'calendar' => $calendar,
             ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Sala  $sala
      * @return \Illuminate\Http\Response
      */
     public function edit(Sala $sala)
@@ -110,15 +103,15 @@ class SalaController extends Controller
 
         return view('sala.edit', [
             'sala' => $sala,
-            'categorias' => Categoria::all()
+            'categorias' => Categoria::all(),
         ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Sala  $sala
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(SalaRequest $request, Sala $sala)
@@ -127,22 +120,23 @@ class SalaController extends Controller
 
         $validated = $request->validated();
         $sala->update($validated);
-        request()->session()->flash('alert-info', 'Sala atualizada com sucesso.');
+        request()->session()->flash('alert-success', 'Sala atualizada com sucesso.');
+
         return redirect("/salas/{$sala->id}");
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Sala  $sala
      * @return \Illuminate\Http\Response
      */
     public function destroy(Sala $sala)
     {
         $this->authorize('admin');
-        
+
         $sala->delete();
-        request()->session()->flash('alert-info', 'Sala excluída com sucesso.');
+        request()->session()->flash('alert-success', 'Sala excluída com sucesso.');
+
         return redirect('/salas');
     }
 }
