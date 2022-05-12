9e132de26f9358f721173c236793045e4d09a520
Saotome <saotome@usp.br>
2022-05-12 14:56:21 -0300

    Melhora leiaute do código
    Code sniffer para o arquivo verifyRoomAvailability

--- app/Rules/verifyRoomAvailability.php
+++ app/Rules/verifyRoomAvailability.php
@@ -2,10 +2,10 @@
 
 namespace App\Rules;
 
-use Illuminate\Contracts\Validation\Rule;
 use App\Models\Reserva;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
+use Illuminate\Contracts\Validation\Rule;
 
 class verifyRoomAvailability implements Rule
 {
@@ -13,6 +13,7 @@ class verifyRoomAvailability implements Rule
     private $id;
     private $conflicts = '';
     private $n = 0;
+
     /**
      * Create a new rule instance.
      *
@@ -27,28 +28,31 @@ class verifyRoomAvailability implements Rule
     /**
      * Determine if the validation rule passes.
      *
-     * @param  string  $attribute
-     * @param  mixed  $value
+     * @param string $attribute
+     * @param mixed  $value
+     *
      * @return bool
      */
     public function passes($attribute, $value)
     {
         $this->check($value);
 
-        if($this->reserva->repeat_days && $this->reserva->repeat_until){
-            # TODO: temos que checar que $fim é maior que $inicio
+        if ($this->reserva->repeat_days && $this->reserva->repeat_until) {
             $inicio = Carbon::createFromFormat('d/m/Y', $value);
             $fim = Carbon::createFromFormat('d/m/Y', $this->reserva->repeat_until);
             $period = CarbonPeriod::between($inicio, $fim);
 
             foreach ($period as $date) {
-                if(in_array($date->dayOfWeek, $this->reserva->repeat_days)){
+                if (in_array($date->dayOfWeek, $this->reserva->repeat_days)) {
                     $this->check($date->format('d/m/Y'));
                 }
             }
         }
 
-        if($this->n != 0) return false; 
+        if ($this->n != 0) {
+            return false;
+        }
+
         return true;
     }
 
@@ -62,27 +66,30 @@ class verifyRoomAvailability implements Rule
         return "Reserva não foi criada porque conflita com: <ul>{$this->conflicts}</ul>";
     }
 
-    private function check($day){
-        # 1. Vamos pegar as reservas que existem para o mesmo dia e mesmo horário
+    private function check($day)
+    {
+        // 1. Vamos pegar as reservas que existem para o mesmo dia e mesmo horário
         $data = Carbon::createFromFormat('d/m/Y', $day);
-        $reservas = Reserva::whereDate('data','=',$data)->where('sala_id',$this->reserva->sala_id)->get();
+        $reservas = Reserva::whereDate('data', '=', $data)->where('sala_id', $this->reserva->sala_id)->get();
 
-        # 2. Se não há reserva alguma na data e sala em questão, podemos cadastrar
-        if($reservas->isEmpty()) return true;
+        // 2. Se não há reserva alguma na data e sala em questão, podemos cadastrar
+        if ($reservas->isEmpty()) {
+            return true;
+        }
 
-        # 3. Se há conflitos vamos montar a string $conflicts indicando-os
-        $inicio = Carbon::createFromFormat('d/m/Y H:i', $day . ' ' .$this->reserva->horario_inicio);
-        $fim    = Carbon::createFromFormat('d/m/Y H:i', $day . ' ' .$this->reserva->horario_fim);
+        // 3. Se há conflitos vamos montar a string $conflicts indicando-os
+        $inicio = Carbon::createFromFormat('d/m/Y H:i', $day.' '.$this->reserva->horario_inicio);
+        $fim = Carbon::createFromFormat('d/m/Y H:i', $day.' '.$this->reserva->horario_fim);
 
         $desejado = CarbonPeriod::between($inicio, $fim);
 
-        foreach($reservas as $reserva){
+        foreach ($reservas as $reserva) {
             $period = CarbonPeriod::between($reserva->inicio, $reserva->fim);
-            if($period->overlaps($desejado)) {
-                # vamos ignorar a própria reserva
-                if($this->id != $reserva->id){
+            if ($period->overlaps($desejado)) {
+                // vamos ignorar a própria reserva
+                if ($this->id != $reserva->id) {
                     $this->conflicts .= "<li><a href='/reservas/{$reserva->id}'>$reserva->nome</a></li>";
-                    $this->n++;
+                    ++$this->n;
                 }
             }
         }
