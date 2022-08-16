f4b51b0af5d4ed7c0371b2141a52c9fe75353349
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2022-08-16 18:57:41 -0300

    implementa audit

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Categoria extends Model
+class Categoria extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+    
     use HasFactory;
     protected $guarded = ['id'];
 
