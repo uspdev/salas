29e2e1d036afdaae19d1e221c3dafda8eb719529
Kawan Santana <kawansantana18@gmail.com>
2023-11-27 14:58:55 -0300

    Criando variável de aplicação 'emailConfigurado'

--- config/salas.php
+++ config/salas.php
@@ -10,5 +10,8 @@ return [
     'cores' => [
         'pendente' => '#FFB540',
         'semFinalidade' => '#BDBDBD'
-    ]
+    ],
+    'emailConfigurado' => (env('MAIL_MAILER') != null   && env('MAIL_HOST') != null 
+                        && env('MAIL_PORT') != null     && env('MAIL_USERNAME') != null     
+                        && env('MAIL_PASSWORD') != null && env('MAIL_ENCRYPTION') != null)
 ];
