e4383f5e509f59d7c3228c93ecf0480754449252
Kawan Santana <kawansantana18@gmail.com>
2023-12-12 11:20:23 -0300

    Criando tabela 'responsaveis_reservas'

---
+++ database/migrations/2023_12_12_111306_create_reservas_responsaveis_reservas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservasResponsaveisReservasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservas_responsaveis_reservas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('reserva_id')->unsigned();
+            $table->foreign('reserva_id')->references('id')->on('reservas');
+            $table->foreignId('responsavel_reserva_id')->constrained('responsaveis_reservas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservas_responsaveis_reservas');
+    }
+}
