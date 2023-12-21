2d202c16d6a66a93ac6152e08183cc48ef01163d
Kawan Santana <kawansantana18@gmail.com>
2023-12-21 14:37:36 -0300

    Unificando coluna 'Ações' e 'Modificar Sala'

--- resources/views/sala/listar.blade.php
+++ resources/views/sala/listar.blade.php
@@ -49,10 +49,7 @@
                             <th>Nome da Sala</th>
                             <th>Categoria</th>
                             <th>Capacidade</th>
-                            <th class="col-sm-1">Ações</th>
-                            @can('admin')
-                            <th class="col-sm-1 text-right">Modificar Sala</th>
-                            @endcan
+                            <th class="col-sm-2">Ações</th>
                         </tr>
                     </thead>
                     @forelse($salas as $sala)
@@ -60,22 +57,28 @@
                             <td><a @can('admin') href="{{route('salas.edit', $sala->id)}}" @endcan>{{ $sala->nome }}</a></td>
                             <td>{{$sala->categoria->nome}}</td>
                             <td>{{$sala->capacidade}} pessoas</td>
-                            <td> <a href="{{route('salas.show', $sala->id)}}" title="Calendário da sala"><i class="fa fa-calendar fa-lg"></i></a> @if(Gate::allows('members', $sala->id) && !($sala->restricao->bloqueada ?? false)) <a href="{{route('reservas.create', ['sala' => $sala->id])}}" title="Cadastrar reserva na sala"><i class="fa fa-calendar-plus fa-lg ml-2"></i></a>@endif</td>
-                            @can('admin')
-                            <td class="text-right">
-                                <form method="POST" action="{{route('salas.destroy', $sala->id)}}">
-                                    <a class="btn btn-success" href="{{route('salas.edit', $sala->id)}}" role="button"
-                                        data-bs-toggle="tooltip" title="Editar">
-                                        <i class="fa fa-pen"></i>
-                                    </a>
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Tem certeza?');">
-                                        <i class="fa fa-trash"></i>
-                                    </button>
-                                </form>
+                            <td> 
+                                @can('admin')
+                                    <form method="POST" action="{{route('salas.destroy', $sala->id)}}" class="d-inline">
+                                        <a class="btn btn-success" href="{{route('salas.edit', $sala->id)}}" role="button"
+                                            data-bs-toggle="tooltip" title="Editar">
+                                            <i class="fa fa-pen"></i>
+                                        </a>
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger" onclick="return confirm('Tem certeza?');">
+                                            <i class="fa fa-trash"></i>
+                                        </button>
+                                    </form>
+                                @endcan
+
+                                <a href="{{route('salas.show', $sala->id)}}" title="Calendário da sala" class="btn btn-info"><i class="fa fa-calendar"></i></a> 
+
+                                @if(Gate::allows('members', $sala->id) && !($sala->restricao->bloqueada ?? false)) 
+                                    <a href="{{route('reservas.create', ['sala' => $sala->id])}}" title="Cadastrar reserva na sala" class="btn btn-primary"><i class="fa fa-calendar-plus"></i></a>
+                                @endif
+
                             </td>
-                            @endcan
                         </tr>
                     @empty
                         <p>Não há salas cadastradas ainda.</p>
