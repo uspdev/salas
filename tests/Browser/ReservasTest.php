8a7c9b188c31b8d6406fe9a01083cddb94a17fb6
Marcelo Nascimento <marcelojunior@usp.br>
2024-03-12 10:29:42 -0300

    fix: aprova reservas (parcial)

--- tests/Browser/ReservasTest.php
+++ tests/Browser/ReservasTest.php
@@ -74,13 +74,13 @@ class ReservasTest extends DuskTestCase {
 
 
     /**
-     * Aprovar reserva
+     * Aprovar reserva sem privilegio
      *
      * @return void
      * @depends testAcessoComoAdministrador
      */
 
-     public function testAprovarReserva () {
+     public function testAprovarReservaSemPrivilegio () {
 
         // Aprova com conta generica
         $this->browse(function (Browser $browser) {
@@ -89,12 +89,37 @@ class ReservasTest extends DuskTestCase {
                     ->assertSee('403');
         });
 
-        // Aprova com conta do dono da reserva
+        // Aprova com conta do dono da reserva sem privilegio
         $this->browse(function (Browser $browser) {
             $browser->driver->manage()->deleteAllCookies();
             $this->loginUserDonoReserva();
             $browser->visit('/reservas/1/aprovar')
-                    ->assertSee('Reserva aprovada com sucesso.');
+                    ->assertSee('403');
+        });
+    }
+
+
+    /**
+     * Aprovar reserva com privilegio
+     *
+     * @return void
+     * @depends testAcessoComoAdministrador
+     */
+
+     public function testAprovarReservaComPrivilegio () {
+
+        // Login como administrador
+        $this->browse(function (Browser $browser) {
+            $this->loginUserAdm();
+            $browser->loginAs(User::where('codpes', 1)->first())
+                    ->visit('/reservas/my')
+                    ->assertSee('Minhas Reservas');
+        });
+
+        // Aprova com conta do dono da reserva sem privilegio
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/reservas/1/aprovar')
+                    ->assertSee('403');
         });
     }
 
@@ -106,8 +131,42 @@ class ReservasTest extends DuskTestCase {
      * @depends testAcessoComoAdministrador
      */
 
-     public function testCriaReserva () {
+     public function testCriaReservaValida () {
         
+        // Login como administrador
+        $this->browse(function (Browser $browser) {
+            $this->loginUserAdm();
+            $browser->loginAs(User::where('codpes', 1)->first())
+                    ->visit('/reservas/my')
+                    ->assertSee('Minhas Reservas');
+        });
+
+        // Cria uma reserva valida
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/reservas/create')
+                    ->type('nome', 'reserva_teste')
+                    ->type('data', '14/01/2021')
+                    ->type('horario_inicio', '8:00')
+                    ->type('horario_fim', '9:00')
+                    ->select('sala_id', 1)
+                    ->type('descricao', 'reserva_teste')
+                    ->press('Enviar')
+                    ->assertPathIs('/reservas')
+                    ->visit('/reservas/2')
+                    ->assertSee('reserva_teste');
+        });
+     }
+    
+    
+
+    /**
+     * Cria uma reserva
+     *
+     * @return void
+     * @depends testCriaReservaValida
+     */
+    public function testCriaReservaInvalida () {
+
         // Login como administrador
         $this->browse(function (Browser $browser) {
             $this->loginUserAdm();
@@ -172,23 +231,10 @@ class ReservasTest extends DuskTestCase {
                     ->press('Enviar')
                     ->assertPathIs('/reservas/create');
         });
+    }
 
-        // Cria uma reserva valida
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/reservas/create')
-                    ->type('nome', 'reserva_teste')
-                    ->type('data', '14/01/2021')
-                    ->type('horario_inicio', '8:00')
-                    ->type('horario_fim', '9:00')
-                    ->select('sala_id', 1)
-                    ->type('descricao', 'Teste de reserva')
-                    ->press('Enviar')
-                    ->assertPathIs('/reservas')
-                    ->visit('/reservas/2')
-                    ->assertSee('reserva_testes');
-        });
-     }
-    
+    // Edita reserva
+    // Exclui reserva
 
 
 
