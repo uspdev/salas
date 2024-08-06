be5a20b0904b3ad02ba336243014d565b6a47679
Kawan Santana <kawansantana18@gmail.com>
2024-08-06 13:27:09 -0300

    Modal de exclusão

--- resources/views/reserva/show.blade.php
+++ resources/views/reserva/show.blade.php
@@ -2,3 +2,14 @@
 @section('content')
   @include('reserva.partials.fields')
 @endsection  
+
+@section('javascripts_bottom')
+<script>
+  $(document).ready(function(){
+    $('.btn-purge').on('click', function(e) {
+      let purge = $(e.target).data('purge');
+      $('#purge').val(purge);
+    });
+  });
+</script>
+@endsection
