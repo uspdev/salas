37be18b5caca628f55ad697996bb2e4a66f6f7ca
Kawan Santana <kawansantana18@gmail.com>
2024-08-19 12:22:22 -0300

    Trazendo id das reservas

--- app/Http/Resources/V1/ReservaResource.php
+++ app/Http/Resources/V1/ReservaResource.php
@@ -15,6 +15,7 @@ class ReservaResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'nome' => $this->nome,
             'sala' => $this->sala->nome,
             'sala_id' => $this->sala->id,
