8d37e90d65436e5db72e739d8eedde8854063146
Raphael <raphael.feitosa11@gmail.com>
2026-03-04 15:26:45 -0300

    alterando valores

--- tests/Browser/ReservaEditTest.php
+++ tests/Browser/ReservaEditTest.php
@@ -21,21 +21,25 @@ class ReservaEditTest extends DuskTestCase
         $this->browse(function (Browser $browser) use ($user){
             $browser->loginAs($user)
             ->visit('/reservas/create')
-            ->typeSlowly('nome','Reunião STI',1)
-            ->typeSlowly('data','01/01/2026',1)
+            ->pause(2000)
+            ->typeSlowly('nome','Reunião STI',100)
+            ->typeSlowly('data','01/01/2026',100)
             ->clickAtXPath('//body')
-            ->typeSlowly('horario_inicio','8:00',1)
-            ->typeSlowly('horario_fim','10:00',1)
+            ->typeSlowly('horario_inicio','8:00',100)
+            ->typeSlowly('horario_fim','10:00',100)
             ->radio('rep_bool','Sim')
             ->check('repeat_days[1]')
             ->check('repeat_days[2]')
             ->check('repeat_days[3]')
             ->check('repeat_days[4]')
             ->check('repeat_days[5]')
-            ->typeSlowly('repeat_until','31/01/2026',1)
+            ->typeSlowly('repeat_until','31/01/2026',100)
             ->clickAtXPath('//body')
-            ->pause(300)
-            ->click('button[type="submit"]');
+            ->pause(400)
+            ->click('button[type="submit"]')
+            ->pause(2000)
+            ->assertSee('Reserva(s) realizada(s) com sucesso.')
+            ->pause(2000);
         });
     }
 
@@ -44,21 +48,24 @@ class ReservaEditTest extends DuskTestCase
         $user = User::where('id',1023)->first();
         $this->browse(function (Browser $browser) use ($user){
             $browser->loginAs($user)
+            ->pause(2000)
             ->visit('/reservas/create')
-            ->typeSlowly('nome','Aula de Alemão',1)
-            ->typeSlowly('data','01/02/2026',1)
+            ->typeSlowly('nome','Aula de Alemão',100)
+            ->typeSlowly('data','01/02/2026',100)
             ->clickAtXPath('//body')
-            ->typeSlowly('horario_inicio','8:00',1)
-            ->typeSlowly('horario_fim','10:00',1)
+            ->typeSlowly('horario_inicio','8:00',50)
+            ->typeSlowly('horario_fim','10:00',50)
             ->radio('rep_bool','Sim')
             ->check('repeat_days[1]')
             ->check('repeat_days[2]')
             ->check('repeat_days[3]')
             ->check('repeat_days[4]')
             ->check('repeat_days[5]')
-            ->typeSlowly('repeat_until','28/02/2026',1)
+            ->typeSlowly('repeat_until','28/02/2026',100)
             ->clickAtXPath('//body')
-            ->click('button[type="submit"]');
+            ->pause(400)
+            ->click('button[type="submit"]')
+            ->pause(2000);
         });
     }
 
@@ -68,26 +75,27 @@ class ReservaEditTest extends DuskTestCase
         $user = User::where('id',1023)->first();
         $this->browse(function (Browser $browser) use ($user){
             $browser->loginAs($user)
-            ->pause(1000)
             ->click('a[title="Editar"]')
-            ->typeSlowly('nome','Aula de Alemão IV',10)
-            ->typeSlowly('data','01/01/2026',10) // data conflituosa com a primeira reserva!
+            ->assertSee('Atenção: Você está editando um grupo de reservas simultaneamente!')
+            ->typeSlowly('nome','Aula de Alemão IV',100)
+            ->typeSlowly('data','01/01/2026',50) // data conflituosa com a primeira reserva!
             ->pause(2000)
             ->clickAtXPath('//body')
-            ->typeSlowly('horario_inicio','8:00',10)
-            ->typeSlowly('horario_fim','10:00',10)
+            ->typeSlowly('horario_inicio','8:00',100)
+            ->typeSlowly('horario_fim','10:00',100)
             ->radio('rep_bool','Sim')
             ->check('repeat_days[1]')
             ->check('repeat_days[2]')
             ->check('repeat_days[3]')
             ->check('repeat_days[4]')
             ->check('repeat_days[5]')
-            ->typeSlowly('repeat_until','28/02/2026',10)
+            ->pause(400)
+            ->typeSlowly('repeat_until','28/02/2026',100)
             ->clickAtXPath('//body')
             ->click('button[type="submit"]') //erro
             ->pause(2000)
             ->check('skip') // pular as datas com conflito...
-            ->pause(1000)
+            ->pause(4000)
             ->click('button[type="submit"]')
             ->pause(2000);
         });
