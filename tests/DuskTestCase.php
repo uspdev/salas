5c0f775452a43fed00c18320f918a0eca36a4dad
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2026-03-13 00:05:01 -0300

    env testing

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -33,6 +33,8 @@ abstract class DuskTestCase extends BaseTestCase
             $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
             '--disable-search-engine-choice-screen',
             '--disable-smooth-scrolling',
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
         ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
             return $items->merge([
                 '--disable-gpu',
